d64a260fb762dbabd5e0c2d537a2b82daf49d6aa
周民杰 <zhouminjie@ydjdev.com>
2022-10-24 15:38:19 +0800

    添加傻逼注释

--- src/SimpleTask.php
+++ src/SimpleTask.php
@@ -8,33 +8,73 @@
 
 namespace JackChow\SimpleTask;
 
+/**
+ * Class SimpleTask
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\JackChow\SimpleTask\SimpleTaskModel name($name)
+ * @method static \Illuminate\Database\Eloquent\Builder|\JackChow\SimpleTask\SimpleTaskModel creator($creator)
+ *
+ * @package JackChow\SimpleTask
+ */
 class SimpleTask
 {
     public $task;
     public $callback;
 
+    /**
+     * 初始化任务
+     *
+     * @param array $configs
+     *
+     * @return $this
+     */
     public function initialize(array $configs)
     {
         $this->task = app(config('simpletask.model'))::create($configs);
         return $this;
     }
 
+    /**
+     * 处理方法
+     *
+     * @param $job
+     *
+     * @return mixed
+     */
     public function handle($job)
     {
         return $job::dispatch($this->task);
     }
 
+    /**
+     * 获取模型
+     * @return mixed
+     */
     public static function model()
     {
         return app(config('simpletask.model'));
     }
 
-
+    /**
+     * 魔术方法
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
     public function __call($name, $arguments)
     {
         return app(config('simpletask.model'))->$name(...$arguments);
     }
 
+    /**
+     * 魔术方法，静态
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return mixed
+     */
     public static function __callStatic($name, $arguments)
     {
         return self::model()->$name(...$arguments);
--- src/SimpleTaskModel.php
+++ src/SimpleTaskModel.php
@@ -10,37 +10,89 @@ namespace JackChow\SimpleTask;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Class SimpleTaskModel
+ *
+ * @method  static bool IsCreated()
+ * @method  static bool IsProcessing()
+ *
+ * @package JackChow\SimpleTask
+ */
 class SimpleTaskModel extends Model
 {
+    /**
+     * 查询/修改黑名单
+     * @var array
+     */
     protected $guarded = ['id'];
 
+    /**
+     * 数据转换
+     * @var array
+     */
     protected $casts = [
         'params' => 'array'
     ];
 
+    /**
+     * 构造方法，初始化Model
+     *
+     * SimpleTaskModel constructor.
+     *
+     * @param array $attributes
+     */
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->setTable(config('simpletask.table_name'));
     }
 
-
+    /**
+     * 检查任务是否在运行
+     *
+     * @param $query
+     *
+     * @return bool
+     */
     public function scopeIsProcessing($query)
     {
         return $query->where('status', 1)->first() ? true : false;
     }
 
-
+    /**
+     * 检查任务是否已被创建
+     *
+     * @param $query
+     *
+     * @return bool
+     */
     public function scopeIsCreated($query)
     {
         return $query->whereIn('status', [0, 1])->first() ? true : false;
     }
 
+    /**
+     * 全局查询，名称
+     *
+     * @param $query
+     * @param $name
+     *
+     * @return mixed
+     */
     public function scopeName($query, $name)
     {
         return $query->where('name', $name);
     }
 
+    /**
+     * 全局查询，创建者
+     *
+     * @param $query
+     * @param $creatorId
+     *
+     * @return mixed
+     */
     public function scopeCreator($query, $creatorId)
     {
         return $query->where('creator_id', $creatorId);
